f32414b13ea6e869bff699312da6dc85722699e9
sofychars <sofychars@gmail.com>
2026-09-07 21:54:26 -0500

    Update datos.js

--- app/lib/datos.js
+++ app/lib/datos.js
@@ -1,6 +1,9 @@
 import { Redis } from "@upstash/redis";
 
-const redis = Redis.fromEnv();
+const redis = new Redis({
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN,
+});
 
 export async function obtenerCategorias() {
   return (await redis.get("categorias")) || [];
